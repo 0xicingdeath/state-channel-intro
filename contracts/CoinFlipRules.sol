5f1c0d684cc9eaba4e6139a7b9cac8ea8b2a6e19
Natalie Chin <chinnh@mcmaster.ca>
2019-03-15 21:09:10 -0400

    Added a few validation rules on CoinFlip Rules

--- contracts/CoinFlipRules.sol
+++ contracts/CoinFlipRules.sol
@@ -5,9 +5,16 @@ contract CoinFlip {
         address playerOne;
         address playerTwo;
         GameState state;
+        uint256 escrowAmount;
     }
+    mapping (address => uint256) balances;
     mapping (uint256 => Game) allGames;
     enum GameState { NEW, FINISHED }
+
+    modifier isParticipant (uint256 uid) {
+        require(allGames[uid].playerOne == msg.sender || allGames[uid].playerTwo == msg.sender);
+        _;
+    }
 
     modifier isNewGame (uint256 uid) {
         require (allGames[uid].state == GameState.NEW);
@@ -19,11 +26,20 @@ contract CoinFlip {
         _;
     }
 
-    function newGame(uint256 uid, address dst) public {
-        allGames[uid] = Game(msg.sender, dst, GameState.NEW);
+    function deposit() public payable {
+        balances[msg.sender] = msg.value;
     }
 
-    function gameEnded(uint256 uid, address winner) public validPlayer(uid,winner) isNewGame(uid) {
+    function newGame(uint256 uid, address dst, uint256 escrowAmount) public {
+        allGames[uid] = Game(msg.sender, dst, GameState.NEW, escrowAmount);
+    }
+
+    function gameEnded(uint256 uid, address winner) public
+        validPlayer(uid,winner)
+        isNewGame(uid)
+        isParticipant(uid){
+        require (balances[allGames[uid].playerOne] >= allGames[uid].escrowAmount && balances[allGames[uid].playerTwo] >= allGames[uid].escrowAmount);
+
         allGames[uid].state = GameState.FINISHED;
     }
 
